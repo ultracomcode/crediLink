76fc9a69e714550a6676868f3b438b17fbac0f60
Lucas Costa <lukascosta.ls17@gmail.com>
2025-09-13 09:08:25 -0300

    Style: remoção do @Configuration.
    Foi nescessario a remoção, pois estava conflitando com outros arquivos.

--- src/main/java/com/main/CrediLink/itau/config/CertificateConfig.java
+++ src/main/java/com/main/CrediLink/itau/config/CertificateConfig.java
@@ -8,15 +8,13 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import javax.net.ssl.SSLContext;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.security.KeyStore;
 
-@Configuration
+
 public class CertificateConfig {
 
     @Value("${itau.ssl.keystore-path}")
