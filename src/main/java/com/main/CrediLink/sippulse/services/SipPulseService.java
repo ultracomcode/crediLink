ff5bb5d1a63f719b6af431487a5ed30398950ea2
Lucas Costa <lukascosta.ls17@gmail.com>
2025-09-13 09:02:55 -0300

    Style: ajustei a forma como e extraido os dados de autenticação para consumir o SOAP da sippulse.
    foi criado uma nova entity para o cadastrado dos dados para integracao, com isso mapiei um service para consultar esses dados no banco.

--- src/main/java/com/main/CrediLink/sippulse/services/SipPulseService.java
+++ src/main/java/com/main/CrediLink/sippulse/services/SipPulseService.java
@@ -1,11 +1,13 @@
 package com.main.CrediLink.sippulse.services;
 
+import com.main.CrediLink.domain.integrations.entity.IntegrationEntity;
+import com.main.CrediLink.domain.integrations.enums.IntegrationsType;
+import com.main.CrediLink.domain.integrations.service.IntegrationService;
 import com.main.CrediLink.exceptions.SoapIntegrationException;
 import com.main.CrediLink.sippulse.dto.AddCreditDTO;
 import com.main.CrediLink.sippulse.wsdlSippulse.SipPulse;
 import com.main.CrediLink.sippulse.wsdlSippulse.SubscriberWS;
 import com.main.CrediLink.sippulse.wsdlSippulse.UserPrincipalDTO;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.net.MalformedURLException;
@@ -16,22 +18,20 @@ import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseMess
 @Service
 public class SipPulseService {
 
-    @Value("${sippulse.loginPrincipal}")
-    private String login;
+    private final IntegrationService integrationService;
 
-    @Value("${sippulse.passwordPrincipal}")
-    private String password;
+    public SipPulseService(IntegrationService integrationService) {
+        this.integrationService = integrationService;
+    }
 
-    @Value("${sippulse.ws.url}")
-    private String serviceUrl;
 
     private SubscriberWS getSubscriberWSPort() {
         try {
-            URL wsdlUrl = new URL(serviceUrl + "?wsdl");
+            URL wsdlUrl = new URL(integration().getUrlApi() + "?wsdl");
             SipPulse service = new SipPulse(wsdlUrl);
             return service.getSubscriberWSPort();
         } catch (MalformedURLException e) {
-            throw new RuntimeException("URL do serviço inválida: " + serviceUrl, e);
+            throw new RuntimeException("URL do serviço inválida: " + integration().getUrlApi(), e);
         }
     }
 
@@ -55,8 +55,14 @@ public class SipPulseService {
 
     private UserPrincipalDTO createUserPrincipal() {
         UserPrincipalDTO userPrincipal = new UserPrincipalDTO();
-        userPrincipal.setLogin(login);
-        userPrincipal.setPassword(password);
+        userPrincipal.setLogin(integration().getUsername());
+        userPrincipal.setPassword(integration().getPassword());
         return userPrincipal;
     }
+    
+    private IntegrationEntity integration(){
+        return integrationService.findByType(IntegrationsType.SIPPULSE).orElseThrow(
+                () -> new RuntimeException("IntegrationEntity configuration for SIPPULSE not found")
+        );
+    };
 }
