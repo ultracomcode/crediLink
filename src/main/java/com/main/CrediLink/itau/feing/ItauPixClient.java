710374398fe7c041e0528a8b648466bf2bdd29ce
Lucas Costa <lukascosta.ls17@gmail.com>
2025-09-13 09:06:48 -0300

    Style: correção da forma de chama o feingClient para consumir a api do itau.

--- src/main/java/com/main/CrediLink/itau/feing/ItauPixClient.java
+++ src/main/java/com/main/CrediLink/itau/feing/ItauPixClient.java
@@ -1,5 +1,6 @@
 package com.main.CrediLink.itau.feing;
 
+import com.main.CrediLink.itau.config.CertificateConfig;
 import com.main.CrediLink.itau.dto.PixRequest;
 import com.main.CrediLink.itau.dto.responsePix.createPixPaymentDTO;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,13 +9,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "itauPixClient", url = "${itau.api.base-url}")
+@FeignClient(
+        name = "itauPixClient",
+        url = "${itau.api.base-url}",
+        configuration = CertificateConfig.class)
 public interface ItauPixClient {
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/pix_recebimentos/v2/cob",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     createPixPaymentDTO gerarPix(
             @RequestHeader("Authorization") String authorization,
-            @RequestHeader("x-itau-apikey") String apiKey,
             @RequestHeader("x-itau-correlationID") String correlationId,
             @RequestHeader("x-itau-flowID") String flowId,
             @RequestBody PixRequest pixRequest
