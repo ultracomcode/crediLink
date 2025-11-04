617045fe7bd37f8f033cc11b54ccedc14678c8d9
lucassantos <lukascosta.ls17@gmail.com>
2025-11-04 14:19:51 -0300

    test: ajuste no teste unitario de usuarios

--- src/test/java/com/main/CrediLink/UserServiceTest.java
+++ src/test/java/com/main/CrediLink/UserServiceTest.java
@@ -6,27 +6,34 @@ import com.main.CrediLink.application.user.UserService;
 import com.main.CrediLink.application.user.dto.ResponseUserDTO;
 import com.main.CrediLink.application.user.enuns.StatusUser;
 import com.main.CrediLink.application.user.enuns.UserRole;
+import com.main.CrediLink.application.user.exceptions.UserException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class UserServiceTest {
+class UserServiceTest {
+
     @Mock
     private UserRepository userRepository;
 
     @InjectMocks
     private UserService userService;
 
-    public UserServiceTest() {
+    @BeforeEach
+    void setUp() {
         MockitoAnnotations.openMocks(this);
     }
 
     @Test
     void deveBuscarUsuarioPorId() {
+        // Arrange
         UserEntity user = new UserEntity();
         user.setId(1L);
         user.setName("Lucas");
@@ -35,11 +42,27 @@ public class UserServiceTest {
         user.setStatus(StatusUser.A);
         user.setRole(UserRole.ADMIN);
 
-        when(userRepository.findById(1L)).thenReturn(java.util.Optional.of(user));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
 
         ResponseUserDTO result = userService.findById(1L);
 
         assertEquals("Lucas", result.name());
+        verify(userRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void deveRetornarErroAoBuscarUsuarioPorId() {
+
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        RuntimeException exception = assertThrows(
+                UserException.class,
+                () -> userService.findById(1L),
+                "Deve lançar exceção quando o usuário não é encontrado"
+        );
+
+        assertTrue(exception.getMessage().contains("Usuario Não encontrado"));
         verify(userRepository, times(1)).findById(1L);
     }
 }
