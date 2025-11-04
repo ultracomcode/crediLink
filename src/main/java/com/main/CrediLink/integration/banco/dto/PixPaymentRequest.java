c28016c965bd71edbb83d59455b0c9e87b2f9ec0
lucassantos <lukascosta.ls17@gmail.com>
2025-11-04 14:22:24 -0300

    refactor: remoção da propiedade Calendaderio, pois o venciemnto do pix vai ser para 24h

--- src/main/java/com/main/CrediLink/integration/banco/dto/PixPaymentRequest.java
+++ src/main/java/com/main/CrediLink/integration/banco/dto/PixPaymentRequest.java
@@ -1,10 +1,8 @@
 package com.main.CrediLink.integration.banco.dto;
 
 public record PixPaymentRequest(
-        Calendario calendario,
         Valor valor,
         String chave
 ) {
-    public record Calendario(Integer expiracao) {}
     public record Valor(String original) {}
 }
